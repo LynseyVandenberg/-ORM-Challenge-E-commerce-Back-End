5232f078efca35b16e6012d790c8c1995e39d072
Lynsey Vandenberg <lynsey.vandenberg@gmail.com>
2021-07-18 21:15:14 -0400

    updated tag-routes

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,5 @@
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: "../../.env" });
 // .env file is ignored and contains the login creds
-
-
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
@@ -9,7 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306,
+      port: 3001,
       dialectOptions: {
         decimalNumbers: true,
       },
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,27 +2,101 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
+/////////////////////////////////////////////////////////////////////////////////
+// findAll tags
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include:
+      {
+        model: Product,
+        attributes: ['product_name', 'stock', 'price', 'category_id']
+      },
+  }).then(catDat => res.json(catDat))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+/////////////////////////////////////////////////////////////////////////////////
+// findOne tag by id and include the product data
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include:
+      {
+        model: Product,
+        attributes: ['product_name', 'price', 'stock', 'category_id'] 
+      }
+  }).then(catDat => {
+    if (!catDat) {
+      res.status(404).json({ message: 'No tag associated with this ID!'}); 
+      return; 
+    }
+    res.json(catDat);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
+/////////////////////////////////////////////////////////////////////////////////
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  }).then(catDat => { res.json(catDat)
+    .catch(err => { res.status(500).json(err);
+    });
 });
 
+/////////////////////////////////////////////////////////////////////////////////
+// update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+  Tag.update(
+      {
+        tag_name: req.body.tag_name
+      },
+      {
+        where: {
+          id: req.params.id
+        },
+      }).then((catDat) => {
+        if (!catDat) {
+          res.status(404).json({ message: 'No tag associated with this ID!'}); 
+          return; 
+        }
+        res.json(catDat);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+    });
 
+/////////////////////////////////////////////////////////////////////////////////
+// delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(catDat => {
+    if (!catDat) {
+      res.status(404).json({ message: "No tag associated with this ID!" });
+      return;
+    }
+    res.json(catDat);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 });
 
 module.exports = router;
